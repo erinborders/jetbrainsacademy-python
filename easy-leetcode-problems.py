5e676be47cf00ac5e56251c8a57811859c20219b
Erin Borders <borders16@gmail.com>
2020-09-24 23:08:35 -0400

    second problem context

--- easy-leetcode-problems.py
+++ easy-leetcode-problems.py
@@ -77,7 +77,26 @@ class Solution:
         # if you get to this point, there are no matches in the list so end it
         return None 
 
+# 2. Reverse integer
 
+# Given a 32-bit signed integer, reverse digits of an integer.
+
+# Example 1:
+
+# Input: 123
+# Output: 321
+# Example 2:
+
+# Input: -123
+# Output: -321
+# Example 3:
+
+# Input: 120
+# Output: 21
+# Note:
+# Assume we are dealing with an environment which could only store integers within the 32-bit 
+# signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your 
+# function returns 0 when the reversed integer overflows.
             
             
 
