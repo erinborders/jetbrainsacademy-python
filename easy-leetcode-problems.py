f124f8fff51a856c5cff0a3524ee4575b61a55e2
Erin Borders <borders16@gmail.com>
2020-09-24 23:20:25 -0400

    pseudocoding

--- easy-leetcode-problems.py
+++ easy-leetcode-problems.py
@@ -95,9 +95,17 @@ class Solution:
 # Output: 21
 # Note:
 # Assume we are dealing with an environment which could only store integers within the 32-bit 
-# signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your 
+# signed integer range: [−2^31,  2^31 − 1]. For the purpose of this problem, assume that your 
 # function returns 0 when the reversed integer overflows.
             
-            
+class Solution:
+    def reverse(self, x: int) -> int:
+        # convert integer into list
+        list_x = list(x)
+        # check if list is odd or even length
+        # loop through first half of list
+        for i in range(len(list_x)/2):
+            # save values of item i'm on and mirrored item in variables
+            # equate the list positions to the new values
 
             
